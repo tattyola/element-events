3e7fb8f33c5c7b7ca71c1ee32daa51d247c46e06
Tatsiana A <astrouskaya@yahoo.com>
2022-09-11 14:46:56 -0400

    added onChange for input

--- src/App.js
+++ src/App.js
@@ -9,11 +9,19 @@ function App() {
   const plusNumber = (n) => {
       console.log('Number ', n)
   }
+
+  const onChangeName = (event) => {
+      console.log(event.target.value)
+  }
   return (
     <div className="App">
       <button onClick={helloButton}>My Button</button>
       <button onClick={() => console.log('Plus')}>Plus</button>
       <button onClick={() => plusNumber(123)}>Plus number</button>
+
+        <hr/>
+
+        <input onChange={onChangeName}/>
     </div>
   );
 }
