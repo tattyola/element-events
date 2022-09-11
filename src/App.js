1e52584968e03d20f174f07c81df7ec34726f49d
Tatsiana A <astrouskaya@yahoo.com>
2022-09-11 14:28:14 -0400

    added more events

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ function App() {
   const helloButton = () => {
     console.log('Hello!!!!');
   }
+
+  const plusNumber = (n) => {
+      console.log('Number ', n)
+  }
   return (
     <div className="App">
       <button onClick={helloButton}>My Button</button>
+      <button onClick={() => console.log('Plus')}>Plus</button>
+      <button onClick={() => plusNumber(123)}>Plus number</button>
     </div>
   );
 }
