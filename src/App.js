4301910e75016cf8a4a626072ee8d4e36505df30
Tatsiana A <astrouskaya@yahoo.com>
2022-09-11 17:07:31 -0400

    added onChange for input

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import './App.css';
-
 function App() {
 
-  const helloButton = () => {
-    console.log('Hello!!!!');
-  }
+    const helloButton = () => {
+        console.log('Hello!!!!');
+    }
 
-  const plusNumber = (n) => {
-      console.log('Number ', n)
-  }
+    const plusNumber = (n) => {
+        console.log('Number ', n)
+    }
 
-  const onChangeName = (event) => {
-      console.log(event.target.value)
-  }
-  return (
-    <div className="App">
-      <button onClick={helloButton}>My Button</button>
-      <button onClick={() => console.log('Plus')}>Plus</button>
-      <button onClick={() => plusNumber(123)}>Plus number</button>
-
-        <hr/>
-
-        <input onChange={onChangeName}/>
-    </div>
-  );
+    const onChangeName = (event) => {
+        console.log(event.target.value)
+    }
+    return (
+        <div>
+            <button onClick={helloButton}>My Button</button>
+            <button onClick={() => console.log('Plus')}>Plus</button>
+            <button onClick={() => plusNumber(123)}>Plus number</button>
+            <hr/>
+            <input onChange={onChangeName}/>
+        </div>
+    );
 }
 
 export default App;
